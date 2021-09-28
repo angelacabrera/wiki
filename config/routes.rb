5ded423d7fa690105bef3253fefdd5e144c26536
angela cabrera <angela.angelina@gmail.com>
2021-09-28 15:48:04 -0300

    listo

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'articles#index'
+
+  get 'index', to: 'articles#index'
+  get 'articles/new', to: 'articles#new'
+  get 'articles/:id', to: 'articles#show', as: :article
+  get 'articles/:id/edit', to: 'articles#edit', as: :edit_article
+  
+  post 'articles', to: 'articles#create'
+  patch 'articles/:id', to: 'articles#update'
+  delete 'articles/:id', to: 'articles#destroy', as: :delete_article
 end
